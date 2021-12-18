eeb2bd99edfcb5ace59fc5b91ee3e28005eede60
leny <info@flatland.be>
2021-12-19 00:30:54 +0100

    Implement store in main container

--- src/containers/main.js
+++ src/containers/main.js
@@ -3,23 +3,26 @@
  * /src/containers/main.js
  */
 
-import {useCallback, useState} from "react";
+import {DEBUG} from "core/constants";
+
+import {useThunkedReducer} from "hooks/use-thunked-reducer";
 
 import SettingsContainer from "./settings";
 import GraphContainer from "./graph";
 
+import {initState, reducer, StoreContext} from "store/index";
+
+const {Provider: StoreContextProvider} = StoreContext;
+
 const MainContainer = () => {
-    // TODO: use context for settings
-    const [startPointId, setStartPointId] = useState(null);
-    const handleChangeStartPoint = useCallback(s => {
-        setStartPointId(s);
-    });
+    const [state, dispatch] = useThunkedReducer(reducer, null, initState);
+    DEBUG && console.log("DEBUG:state:", state);
 
     return (
-        <>
-            <SettingsContainer onChangeStartPoint={handleChangeStartPoint} />
-            <GraphContainer startPointId={startPointId} />
-        </>
+        <StoreContextProvider value={{...state, dispatch}}>
+            <SettingsContainer />
+            <GraphContainer />
+        </StoreContextProvider>
     );
 };
 
